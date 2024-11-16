fe85e532395a69eeb5c40b491984028d147ff663
andres <jorquera.ad@gmail.com>
2024-11-16 19:32:30 +0100

    fix: update path for more flexible match

--- src/controllers/helloWorldController.ts
+++
@@ -1,7 +0,0 @@
-import e, { RequestHandler } from "express";
-
-const HelloWordController: RequestHandler = (_req, res) => {
-    res.send('Hello World!');
-}
-
-export default HelloWordController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,10 @@
 import { Router } from 'express';
-import { helloWorldController, healthCheckController } from '@/controllers';
+import { healthCheckController } from '@/controllers';
 import DocsController from './controllers/docsController';
 
 const routes = Router();
 
 routes.get('/health', healthCheckController)
-routes.get('/', helloWorldController);
-routes.get('/docs/:fileName', DocsController);
+routes.get('(*)/:fileName', DocsController);
 
 export default routes;
