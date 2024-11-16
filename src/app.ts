89c9e1e94668524f9bbb56304e72994fee220318
andres <jorquera.ad@gmail.com>
2024-11-16 19:38:58 +0100

    feat: adding logger

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import errorHandler from './midlewares/errorHandler';
 import routes from './routes';
+const morgan = require('morgan');
 
 const app = express();
 
+app.use(morgan('tiny'));
 app.use(express.json());
 app.use(routes);
 app.use(errorHandler);
