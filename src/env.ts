cf7351671320573f588609147781a0c839900227
andres <jorquera.ad@gmail.com>
2024-11-16 23:08:53 +0100

    update

--- src/env.ts
+++ src/env.ts
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT || 3000;
-export const INNER_QUERY_PARAM_PREFIX = process.env.INNER_QUERY_PARAM_PREFIX || 'shuttle-json-';
+export const INNER_QUERY_PARAM_PREFIX = process.env.INNER_QUERY_PARAM_PREFIX || '@';
